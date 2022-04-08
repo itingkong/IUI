87d7ad41683de9d6b1848b4980f69073a8e23890
itingkong <luoqi20131001@163.com>
2022-04-08 22:48:28 +0800

    update: layout

--- src/components/Layout/TLR/index.jsx
+++ src/components/Layout/TLR/index.jsx
@@ -1,6 +1,10 @@
 /**
  * 上、左右结构布局
  * @create 2022.2.3
+ * 
+ * topContent Top 组件内容
+ * leftContent Left 组件内容
+ * rightContent Right 组件内容
  */
 import Top from "../Top";
 import Left from "../Left";
@@ -9,11 +13,11 @@ import styles from "./index.less";
 import { layoutMainHeight } from "utils/";
 
 export default function TLR(props) {
-	const { height = 100, leftContent = null, rightContent = null, ...res } = props;
+	const { height = 100, topContent = null, leftContent = null, rightContent = null, ...res } = props;
 
 	return (
 		<div className={styles.tlr}>
-			<Top height={height}>TOP</Top>
+			<Top height={height}>{topContent}</Top>
 			<div className={styles.tlr_main} style={{ height: layoutMainHeight(height) }}>
 				<Left {...res}>{leftContent}</Left>
 				<Right {...res}>{rightContent}</Right>

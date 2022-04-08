25b3286ec046786304d64e1112300a39422278f8
itingkong <luoqi20131001@163.com>
2022-04-08 22:46:24 +0800

    update: theme

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import ReactDOM from "react-dom";
 import App from "./App";
+import "/static/theme/index.css";
+// require("/src/utils/global.js");
+
+document.body.classList.add("theme-dark");
 
 ReactDOM.render(<App />, document.getElementById("app"));
